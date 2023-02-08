eeedc286eeb5e25a8fdb3faff8a61500ea104346
Shiv Thakar <shivthakar8@gmail.com>
2023-02-08 10:28:26 -0500

    change source code and exe file

--- Automatic File Copier/Auto Copy To Clutch Hub.py
+++ Automatic File Copier/Auto Copy To Clutch Hub.py
@@ -8,27 +8,34 @@ GF9_PATH = "N:\\GF9 Clutch Hub\\Flatness Scans" #? Copy these files to network d
 
 #!Exists Function------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def exists(filepath): #* functions checks if the file already exists in the destination folder ~ GF9_PATH
-
-    pdf_file = filepath.split("\\")[-1] #get only the file name
+    global GF9_PATH
+    pdf_file = path.basename(filepath) #get only the file name
     
     gf9 = glob(GF9_PATH+"\\*.pdf") #get all files in destination folder
-
-    for f in gf9: #iterate through destination folder
-        if(f.split("\\")[-1] == pdf_file): #check if the passed file exists
-            return True #if it exists TRUE
-            
-    return False #if it doesn't exists return FALSE
+    gf9.sort(key=path.getmtime, reverse=True)
+    
+    for f in gf9:
+        if(path.basename(f) == path.basename(filepath)):
+            return 0
+        
+    return 1 #if it doesn't exists return FALSE
 #!Code Body------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def equator_copyPDF():
+    global SRC
     while(True):  #to ensure it always runs in the background
         pdf_files = glob(SRC + "\\*.pdf") #get all PDF files from the source folder
-
+        pdf_files.sort(key=path.getmtime, reverse=True)
         for pdf in pdf_files: #iterate through all files in source folder
-            if(exists(pdf)): #call exists() function to check if the file has already been copied
+
+            check = exists(pdf)
+            
+            if(check == 0): #call exists() function to check if the file has already been copied
                 continue #if it exists, go to next file
             
-            copy(pdf,GF9_PATH) #if it doesn't exists, copy the file
-            print("COPIED - "+path.basename(pdf))
+            else:
+                copy(pdf,GF9_PATH) #if it doesn't exists, copy the file
+                print("COPIED - "+path.basename(pdf))
+        
 #!Main Function Call---------------------------------------------------------------------------------------------------------------------------------------------------------------
 equator_copyPDF()
 #!End of Python Script-------------------------------------------------------------------------------------------------------------------------------------------------------------
