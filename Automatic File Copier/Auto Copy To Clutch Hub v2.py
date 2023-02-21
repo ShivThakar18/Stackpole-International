d87f65d1f35c96d37358e9477c08473846d930d5
Shiv Thakar <94186009+ShivThakar18@users.noreply.github.com>
2023-02-21 14:46:22 -0500

    Update Auto Copy To Clutch Hub v2.py

--- Automatic File Copier/Auto Copy To Clutch Hub v2.py
+++ Automatic File Copier/Auto Copy To Clutch Hub v2.py
@@ -9,9 +9,9 @@ GF9_PATH = "N:\\GF9 Clutch Hub\\Flatness Scans" #? Copy these files to network d
 ARCHIVE = "C:\\Users\\PRISMO1\\Documents\\Flatness Archive"
 
 """ #For Testing
-SRC = "C:\\Users\\vrerecich\\Downloads\\PRISMO1" #? Prismo1 PC Directory - All Equator Reports saved here
-GF9_PATH = "C:\\Users\\vrerecich\\Documents\\GF9 Clutch Hub\\Flatness Scans" #? Copy these files to network drive for easy access 
-ARCHIVE = "C:\\Users\\vrerecich\\Documents\\Flatness Archive" """
+SRC = "C:\\Users\\sthakar\\Downloads\\PRISMO1" #? Prismo1 PC Directory - All Equator Reports saved here
+GF9_PATH = "C:\\Users\\sthakar\\Documents\\GF9 Clutch Hub\\Flatness Scans" #? Copy these files to network drive for easy access 
+ARCHIVE = "C:\\Users\\sthakar\\Documents\\Flatness Archive" """
 #!Code Body------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def equator_copyPDF():
     global SRC, GF9_PATH, ARCHIVE
