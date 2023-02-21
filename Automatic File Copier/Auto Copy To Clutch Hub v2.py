20a75f50d7aef759b339331c540919f0175b6ed4
Shiv Thakar <shivthakar8@gmail.com>
2023-02-21 13:35:29 -0500

    reworked file organization

--- Automatic File Copier/Auto Copy To Clutch Hub v2.py
+++ Automatic File Copier/Auto Copy To Clutch Hub v2.py
@@ -2,50 +2,41 @@
 from glob import glob 
 from shutil import copy,move
 from os import path,rename
-from time import localtime, strftime
+from datetime import datetime
 #!File Paths-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-""" SRC = "C:\\Users\\PRISMO1\\Downloads" #? Prismo1 PC Directory - All Equator Reports saved here
+SRC = "C:\\Users\\PRISMO1\\Downloads" #? Prismo1 PC Directory - All Equator Reports saved here
 GF9_PATH = "N:\\GF9 Clutch Hub\\Flatness Scans" #? Copy these files to network drive for easy access 
-ARCHIVE = "C:\\Users\\PRISMO1\\Documents\\Flatness Archive" """
+ARCHIVE = "C:\\Users\\PRISMO1\\Documents\\Flatness Archive"
 
-#For Testing
-SRC = "C:\\Users\\vrerecich\\Downloads" #? Prismo1 PC Directory - All Equator Reports saved here
+""" #For Testing
+SRC = "C:\\Users\\vrerecich\\Downloads\\PRISMO1" #? Prismo1 PC Directory - All Equator Reports saved here
 GF9_PATH = "C:\\Users\\vrerecich\\Documents\\GF9 Clutch Hub\\Flatness Scans" #? Copy these files to network drive for easy access 
-ARCHIVE = "C:\\Users\\vrerecich\\Documents\\Flatness Archive"
-#!Exists Function------------------------------------------------------------------------------------------------------------------------------------------------------------------
-def exists(filepath): #* functions checks if the file already exists in the destination folder ~ GF9_PATH
-    global GF9_PATH
-    pdf_file = path.basename(filepath) #get only the file name
-    
-    gf9 = glob(GF9_PATH+"\\*.pdf") #get all files in destination folder
-    gf9.sort(key=path.getmtime, reverse=True)
-    
-    for f in gf9:
-        if(path.basename(f) == path.basename(filepath)):
-            return 0
-        
-    return 1 #if it doesn't exists return FALSE
+ARCHIVE = "C:\\Users\\vrerecich\\Documents\\Flatness Archive" """
 #!Code Body------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def equator_copyPDF():
     global SRC
     while(True):  #to ensure it always runs in the background
+        
         pdf_files = glob(SRC + "\\*.pdf") #get all PDF files from the source folder
         pdf_files.sort(key=path.getmtime, reverse=True)
+
         for pdf in pdf_files: #iterate through all files in source folder
 
-            check = exists(pdf)
             
-            if(check == 0): #call exists() function to check if the file has already been copied    
-                continue #if it exists, go to next file
-            
-            else:
-                copy(pdf,GF9_PATH) #if it doesn't exists, copy the file
-                print("COPIED - "+path.basename(pdf))
 
-                '''
-                move files to an archive right away
-                '''
-        
+            new_name = SRC + "\\" + path.basename(pdf)[0:-4] + "_" + datetime.fromtimestamp(path.getmtime(pdf)).strftime('%Y_%m_%d') +'.pdf'
+            print(new_name)
+
+            rename(pdf, new_name)
+            
+            copy(new_name,GF9_PATH) #if it doesn't exists, copy the file
+            print("COPIED - "+path.basename(pdf))
+
+            move(new_name,ARCHIVE)
+
+            '''
+            move files to an archive right away
+            ''' 
 #!Main Function Call---------------------------------------------------------------------------------------------------------------------------------------------------------------
 equator_copyPDF()
 #!End of Python Script-------------------------------------------------------------------------------------------------------------------------------------------------------------
