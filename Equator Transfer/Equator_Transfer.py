2ca5562766a8e43ee946ef7387654aa70a80ecd1
Shiv Thakar <shivthakar8@gmail.com>
2023-03-16 08:27:38 -0400

    update eq transfer with comments

--- Equator Transfer/Equator_Transfer.py
+++ Equator Transfer/Equator_Transfer.py
@@ -1,50 +1,47 @@
-# ---------------------------------------------------------------------------- #
-#                      Equator Gauge File Transfer Program                     #
-# ---------------------------------------------------------------------------- #
-# ------------------------------ Import Modules ------------------------------ #
-from glob import glob
-from shutil import move
-from time import sleep
-# ----------------------------- Define Variables ----------------------------- #
-OUT = "C:\\EQUATOR"
-EQ9879_IN = "P:\\*.RES"
-EQ9875_IN = "T:\\*.RES"
-EQ9877_IN = "W:\\*.RES"
-# ----------------------------- Transfer Function ---------------------------- #
-def transfer(files):
-    for f in files:
-        try: 
-            move(f,OUT)
-            print("FILE MOVED - ",f)
+
+# *--------------------------------------------------- Shiv Thakar --------------------------------------------------- #
+# *--------------------------------------------- Stackpole International --------------------------------------------- #
+# *--------------------------------------- Equator Gauge File Transfer Program --------------------------------------- #
+# *----------------------------------------------- Feburary 8th, 2023 ------------------------------------------------ #
+# !------------------------------------------------- Import Modules -------------------------------------------------- #
+from glob import glob                                               # file directories
+from shutil import move                                             # move files
+from time import sleep                                              # time delay
+# !------------------------------------------------ Define Variables ------------------------------------------------- #
+OUT = "C:\\EQUATOR"                                                 # output folder/QC-Calc Folder
+EQ9879_IN = "P:\\*.RES"                                             # P:\ drive for EQ 9879
+EQ9875_IN = "T:\\*.RES"                                             # T:\ drive for EQ 9875
+EQ9877_IN = "W:\\*.RES"                                             # W:\ drive for EQ 9877
+# !---------------------------------------------------- Functions ---------------------------------------------------- #
+# ?------------------------------------------------ Transfer Function ------------------------------------------------ #
+def transfer(files): #* move files from drive to output folder
+    for f in files:                                                 # iterate through files in list
+        try:                                                        # error sometimes occurs 
+            move(f,OUT)                                             # move each file in the list to the output folder
+            print("FILE MOVED - ",f)                
         except:
             print("ERROR RAISED AND EXCEPTED")
-        sleep(1)
-    return 0
-# ------------------------- End of Transfer Function ------------------------- #
-# ------------------------------- Read Function ------------------------------ #
-def read_folder():
+        sleep(1)                                                    # wait 1 second
+# ?-------------------------------------------------- Read Function -------------------------------------------------- #
+def read_folder():  #* reads files from each drive
 
-    res_files = []
+    res_files = []                                                  # list of files
 
-    p_drive = glob(EQ9879_IN)
-    t_drive = glob(EQ9875_IN)
-    w_drive = glob(EQ9877_IN)
+    p_drive = glob(EQ9879_IN)                                       # files from P:\
+    t_drive = glob(EQ9875_IN)                                       # files from T:\
+    w_drive = glob(EQ9877_IN)                                       # files from W:\
 
-    for p in p_drive:
+    for p in p_drive:                                               # append P:\ files to res_files list
         res_files.append(p)
-    for t in t_drive:
+    for t in t_drive:                                               # append T:\ files to res_files list
         res_files.append(t)
-    for w in w_drive:
+    for w in w_drive:                                               # append W:\ files to res_files list
         res_files.append(w)
-    transfer(res_files)
-
-    return 0
-# --------------------------- End of Read Function --------------------------- #
-# ------------------------------- Function Call ------------------------------ #
-while(True):
-
-    read_folder()
-    sleep(5)
-# ---------------------------------------------------------------------------- #
-#                                End of Program                                #
-# ---------------------------------------------------------------------------- #
+    transfer(res_files)                                             # call transfer function
+# !-------------------------------------------------- Function Call -------------------------------------------------- #
+while(True): #* run infinitely
+    read_folder()                                                   # call read_folder
+    sleep(5)                                                        # wait 5 seconds after one cycle
+# !------------------------------------------------------------------------------------------------------------------- #
+# !                                                  End of Program                                                    #
+# !------------------------------------------------------------------------------------------------------------------- #
